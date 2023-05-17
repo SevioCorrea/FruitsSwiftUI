f2634ffe2a0ed5d0ae1ff7017d2bafd2b50269ac
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-17 18:34:50 -0300

    First Commit

---
+++ FruitsSwiftUI/FruitsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsSwiftUIApp.swift
+//  FruitsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 17/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
