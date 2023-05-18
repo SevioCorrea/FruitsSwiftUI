89a3c55b2b70979647645ba72ceda03edc23fdb6
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-18 13:46:43 -0300

    Starting Design

---
+++ FruitsSwiftUI/View/FruitCardView.swift
@@ -0,0 +1,53 @@
+//
+//  FruitCardView.swift
+//  FruitsSwiftUI
+//
+//  Created by Sévio Basilio Corrêa on 18/05/23.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    
+    // MARK: - Propriedades
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                //: Fruit: Image
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                //: Fruit: Title
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                //: Fruit: Headline
+                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the World.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+                
+                //: Button: Start
+            } //: VStack
+        } //: ZStack
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBluberryDark")]), startPoint: .top, endPoint: .bottom))
+        .cornerRadius(20)
+    }
+}
+
+// MARK: - Preview
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
