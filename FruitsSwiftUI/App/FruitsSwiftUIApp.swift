f7fcac3edbf9eaf1d9a1592f02d3468b391212bf
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-05-22 11:34:43 -0300

    OnboardingView

--- FruitsSwiftUI/App/FruitsSwiftUIApp.swift
+++ FruitsSwiftUI/App/FruitsSwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct FruitsSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            OnboardingView()
         }
     }
 }
